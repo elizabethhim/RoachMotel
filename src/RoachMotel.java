0f12263b2e35e327a0deb72dd110e33ae0cfed2a
ricardoalcaraz <ralcarazerhs@gmail.com>
2017-11-29 15:25:18 -0800

    Added a hashmap to assign the colonies to a room number

--- src/RoachMotel.java
+++ src/RoachMotel.java
@@ -2,10 +2,10 @@
    Purpose: this class represents a Roach Motel
             and uses the Singleton Design Pattern to limit motel creation
  */
-
+import java.util.HashMap;
 //TODO: add methods, if necessary
-
 public class RoachMotel {
+
     /**an instance of Roach Motel*/
     private static RoachMotel motel;
 
@@ -15,12 +15,16 @@ public class RoachMotel {
     /**vacancy condition of Roach Motel*/
     private boolean vacancy;
 
+    /**List of Roach colonies*/
+    private HashMap<Integer, RoachColony> rooms;
+
     /**private constructor of a motel to prevent new motel creation
      * initial capacity set at five rooms
      * initial vacancy set at true*/
     private RoachMotel() {
         capacity = 5;
         vacancy = true;
+        rooms = new HashMap<Integer, RoachColony>();
     }
 
     /**creates new instances of a Roach Motel
@@ -59,5 +63,20 @@ public class RoachMotel {
      */
     public void setVacancy(boolean vacancy) {
         this.vacancy = vacancy;
+    }
+
+    /**Set a room to a roach colony
+     * @param room number
+     * @param guest RoachColony that wants to stay
+     */
+    public void setRoom( int room, RoachColony guest) {
+        rooms.put(room, guest);
+    }
+
+    /**Clear a roach colony from a room
+     * @param roomNumber to clear from motel
+     */
+    public void clearRoom( int roomNumber ) {
+        rooms.remove( roomNumber );
     }
 }
